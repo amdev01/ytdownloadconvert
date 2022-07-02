6965000344a39584f26d3bee508d546f02d991bc
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2022-07-02 23:41:55 +0100

    Fix download from file

--- download_audio_yt.py
+++ download_audio_yt.py
@@ -8,22 +8,23 @@ import re
 import sys
 
 usage = """\
-Usage: download_audio_yt.py [-hul] "<url/textfile_with_urls>" [-p] "<download_path>"
+Usage: download_audio_yt.py [-h|u|l|c|p] "<url/textfile_with_urls/music_path>" [-c|p] "<music_path>" [-c]
   -h/--help             - <optional> print this manual
-  -u/--url              - <required> download audio from <url>
-  -l/--list             - <required> download audio list of playlists at <textfile_with_urls>
+  -u/--url              - <required> download audio from playlist <url>
+  -l/--list             - <required> download audio from a list of playlists at <textfile_with_urls>
+  -p/--path             - <optional> specify custom download location
+  -c                    - <optional> use to convert downloaded mp4s to mp3s
   <url>                 - <required> playlist url "https://www.youtube.com/playlist?list=xxx"
   <textfile_with_urls>  - <required> path to text file holding all urls of playlists to download
-  -p/--path             - <optional> specify custom download location
-  <download_path>       - <optional> path to custom download folder\
+  <music_path>          - <optional> path to custom download folder\
 """
 
 
 # DOWNLOAD PLAYLIST AND CONVERT TO AUDIO
 def download_from_yt(playlist: Playlist, folder: str):
     # DOWNLOAD ALL MP4S IN PLAYLIST
-    print("Downloading %i items from playlist" % playlist.length)
-    with alive_bar(playlist.length, dual_line=True) as bar:
+    print("Downloading %i items from playlist" % len(playlist))
+    with alive_bar(len(playlist), dual_line=True) as bar:
         for song_url in playlist:
             title = YouTube(song_url).title
             # YouTube(song_url).streams.filter(only_audio=True).first().download(folder)
@@ -51,55 +52,81 @@ def convert_to_mp3(folder: str):
             os.remove(mp4_path)
 
 
+def setargs_download(playlist_url: str, folder: str, convert: bool):
+    playlist = Playlist(playlist_url)
+    folder = os.path.join(folder, playlist.title)
+    download_from_yt(playlist, folder)
+    if convert:
+        convert_to_mp3(folder)
+
+
+def invalid_url_err(playlist_url: str):
+    if not validators.url(playlist_url):
+        print("URL %s not valid, quitting..." % playlist_url)
+        sys.exit(1)
+
+
+def inlavid_file_err(file: str):
+    if not os.path.isfile(file):
+        print("Playlist URL list %s does not exist..." %
+              file)
+        sys.exit(1)
+
+
+def invalid_path_err(file: str):
+    if not os.path.exists(file):
+        print("Specified output folder %s does not exist..." %
+              file)
+        sys.exit(1)
+
+
 def cli():
     # EXIT IF NONE OR TOO MANY ARGUMENTS
-    if len(sys.argv) < 2 or len(sys.argv) > 5:
+    if len(sys.argv) < 2 or len(sys.argv) > 6:
         print(usage)
         sys.exit(1)
     # PRINT HELP MENU
     if sys.argv[1] in ["-h", "--help"]:
         print(usage)
         sys.exit(0)
-    # CHECK IF CUSTOM PATH SPECIFIED
+    convert = False
     folder = "Music"
+    # Convert mp4s in current dir to mp3s
+    if sys.argv[1] in ["-c", "--convert"]:
+        convert_to_mp3(folder)
+        sys.exit(0)
+    # Check if custom path set with convert flag
     if len(sys.argv) > 3:
+        if sys.argv[1] in ["-p", "--path"] and sys.argv[3] in ["-c", "--convert"]:
+            invalid_path_err(sys.argv[2])
+            convert_to_mp3(sys.argv[2])
+            sys.exit(0)
+        # Check if path and list/url are set
         if sys.argv[3] in ["-p", "--path"]:
-            if not os.path.exists(sys.argv[4]):
-                print("Specified output folder %s does not exist..." %
-                      sys.argv[4])
-                sys.exit(1)
+            invalid_path_err(sys.argv[4])
             folder = sys.argv[4]
+        if sys.argv[3] in ["-c", "--convert"] or sys.argv[5] in ["-c", "--convert"]:
+            convert = True
     # DOWNLOAD FROM URL
     if sys.argv[1] in ["-u", "--url"]:
-        if not validators.url(sys.argv[2]):
-            print("URL %s not valid, quitting..." % sys.argv[2])
-            sys.exit(1)
-        playlist = Playlist(sys.argv[2])
-        folder = os.path.join(folder, playlist.title)
-        download_from_yt(playlist, folder)
-        convert_to_mp3(folder)
+        invalid_url_err(sys.argv[2])
+        setargs_download(sys.argv[2], folder, convert)
+        sys.exit(0)
     # DOWNLOAD FROM LIST OF URLS IN A TEXT FILE
     if sys.argv[1] in ["-l", "--list"]:
-        if not os.path.isfile(sys.argv[2]):
-            print("Playlist URL list %s does not exist..." %
-                  sys.argv[2])
-            sys.exit(1)
+        inlavid_file_err(sys.argv[2])
         url_list = open(sys.argv[2]).readlines()
         for playlist_url in url_list:
-            if not validators.url(playlist_url):
-                print("url %s not valid, quitting..." % sys.argv[2])
-                sys.exit(1)
-            playlist = Playlist(sys.argv[2])
-            folder = os.path.join(folder, playlist.title)
-            download_from_yt(playlist, folder)
-            convert_to_mp3(folder)
-
+            invalid_url_err(playlist_url)
+            setargs_download(playlist_url, folder, convert)
+        sys.exit(0)
 
 # TODO
 # redo CLI to:
-# add -c at the end to convert, will only download by default
-# add -c to the path without -u/-l to only convert in specified path
-# add -c without any other command to automatically convert all files under current dir
+# add -c at the end to convert, will only download by default [+]
+# add -c to the path without -u/-l to only convert in specified path [+]
+# add -c without any other command to automatically convert all files under current dir [+]
+# when downloading from a list of playlist, download and convert in parallel [-]
 
 
 if __name__ == '__main__':
