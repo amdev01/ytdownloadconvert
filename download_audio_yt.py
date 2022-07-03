4be1c495c5c6ed0fa67354b4f730bf389e5e453b
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2022-07-03 01:44:22 +0100

    Further improvements

--- download_audio_yt.py
+++ download_audio_yt.py
@@ -8,20 +8,22 @@ import re
 import sys
 
 usage = """\
-Usage: download_audio_yt.py [-h|u|l|c|p] "<url/textfile_with_urls/music_path>" [-c|p] "<music_path>" [-c]
-  -h/--help             - <optional> print this manual
-  -u/--url              - <required> download audio from playlist <url>
-  -l/--list             - <required> download audio from a list of playlists at <textfile_with_urls>
-  -p/--path             - <optional> specify custom download location
-  -c                    - <optional> use to convert downloaded mp4s to mp3s
-  <url>                 - <required> playlist url "https://www.youtube.com/playlist?list=xxx"
-  <textfile_with_urls>  - <required> path to text file holding all urls of playlists to download
-  <music_path>          - <optional> path to custom download folder\
+Usage: download_audio_yt.py [-h|c|u|l|p] "<music_path/url/urls.txt>" [OPTIONAL][-c|p] "<music_path>" [-c]
+  -h/--help             - print this manual
+  -u/--url              - download mp4 from playlist <url>
+  -f/--file             - download mp4 from a list of playlists at <urls.txt>
+  -p/--path             - specify custom download / music conversion location
+  -c/--convert          - use at the end to convert downloaded mp4s to mp3s
+     |--                 - use with -p to convert all mp4s in specified folder
+     |--                 - use as only argument to convert all mp4s in /Music
+  <url>                 - playlist url "https://www.youtube.com/playlist?list=xxx"
+  <urls.txt>            - path to text file holding all urls of playlists to download
+  <music_path>          - path to custom download folder\
 """
 
 
 # DOWNLOAD PLAYLIST AND CONVERT TO AUDIO
-def download_from_yt(playlist: Playlist, folder: str):
+def download_mp4_from_yt(playlist: Playlist, folder: str):
     # DOWNLOAD ALL MP4S IN PLAYLIST
     print("Downloading %i items from playlist" % len(playlist))
     with alive_bar(len(playlist), dual_line=True) as bar:
@@ -33,13 +35,14 @@ def download_from_yt(playlist: Playlist, folder: str):
                 YouTube(song_url).streams.filter(
                     file_extension='mp4').first().download(folder)
             except KeyError:
+                # Download with oauth if content requires age verification
                 bar.text = f'Download of {title} failed, re-trying with oauth...'
                 YouTube(song_url, use_oauth=True, allow_oauth_cache=True).streams.filter(
                     file_extension='mp4').first().download(folder)
             bar()
 
 
-def convert_to_mp3(folder: str):
+def convert_mp4_to_mp3(folder: str):
     # LOOP THROUGH LIST DOWNLOADED FILES AND CONVERT TO MP3
     for file in os.listdir(folder):
         if re.search('mp4', file):
@@ -52,12 +55,9 @@ def convert_to_mp3(folder: str):
             os.remove(mp4_path)
 
 
-def setargs_download(playlist_url: str, folder: str, convert: bool):
+def join_playlist_folder_to_path(playlist_url: str, folder: str):
     playlist = Playlist(playlist_url)
-    folder = os.path.join(folder, playlist.title)
-    download_from_yt(playlist, folder)
-    if convert:
-        convert_to_mp3(folder)
+    return os.path.join(folder, playlist.title)
 
 
 def invalid_url_err(playlist_url: str):
@@ -89,43 +89,51 @@ def cli():
     if sys.argv[1] in ["-h", "--help"]:
         print(usage)
         sys.exit(0)
-    convert = False
     folder = "Music"
-    # Convert mp4s in current dir to mp3s
+    # Convert mp4s in Music/ dir to mp3s
     if sys.argv[1] in ["-c", "--convert"]:
-        convert_to_mp3(folder)
+        convert_mp4_to_mp3(folder)
         sys.exit(0)
     # Check if custom path set with convert flag
     if len(sys.argv) > 3:
         if sys.argv[1] in ["-p", "--path"] and sys.argv[3] in ["-c", "--convert"]:
             invalid_path_err(sys.argv[2])
-            convert_to_mp3(sys.argv[2])
+            convert_mp4_to_mp3(sys.argv[2])
             sys.exit(0)
+    # DOWNLOAD FROM URL
+    if sys.argv[1] in ["-u", "--url"]:
+        invalid_url_err(sys.argv[2])
+        if sys.argv[3] in ["-p", "--path"]:
+            invalid_path_err(sys.argv[4])
+            folder = sys.argv[4]
+        download_mp4_from_yt(
+            Playlist(sys.argv[2]), join_playlist_folder_to_path(sys.argv[2], folder))
+        if sys.argv[3] in ["-c", "--convert"] or sys.argv[5] in ["-c", "--convert"]:
+            convert_mp4_to_mp3(
+                join_playlist_folder_to_path(sys.argv[2]), folder)
+        sys.exit(0)
+    # DOWNLOAD FROM LIST OF URLS IN A TEXT FILE
+    if sys.argv[1] in ["-f", "--file"]:
+        inlavid_file_err(sys.argv[2])
+        url_list = open(sys.argv[2]).readlines()
         # Check if path and list/url are set
         if sys.argv[3] in ["-p", "--path"]:
             invalid_path_err(sys.argv[4])
             folder = sys.argv[4]
-        if sys.argv[3] in ["-c", "--convert"] or sys.argv[5] in ["-c", "--convert"]:
-            convert = True
-    # DOWNLOAD FROM URL
-    if sys.argv[1] in ["-u", "--url"]:
-        invalid_url_err(sys.argv[2])
-        setargs_download(sys.argv[2], folder, convert)
-        sys.exit(0)
-    # DOWNLOAD FROM LIST OF URLS IN A TEXT FILE
-    if sys.argv[1] in ["-l", "--list"]:
-        inlavid_file_err(sys.argv[2])
-        url_list = open(sys.argv[2]).readlines()
         for playlist_url in url_list:
             invalid_url_err(playlist_url)
-            setargs_download(playlist_url, folder, convert)
+            download_mp4_from_yt(
+                Playlist(playlist_url), join_playlist_folder_to_path(playlist_url, folder))
+            if sys.argv[3] in ["-c", "--convert"] or sys.argv[5] in ["-c", "--convert"]:
+                convert_mp4_to_mp3(
+                    join_playlist_folder_to_path(playlist_url), folder)
         sys.exit(0)
 
 # TODO
 # redo CLI to:
 # add -c at the end to convert, will only download by default [+]
 # add -c to the path without -u/-l to only convert in specified path [+]
-# add -c without any other command to automatically convert all files under current dir [+]
+# add -c w/ other command to auto convert all files under Music dir [+]
 # when downloading from a list of playlist, download and convert in parallel [-]
 
 
